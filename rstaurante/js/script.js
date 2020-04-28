f2e4e9474d86a8e11195aff0fe1a367dce3e6f85
littlejhon001 <62266656+littlejhon001@users.noreply.github.com>
2020-04-27 21:32:39 -0500

    actualizacion web
    actualizacion web 27/04/20

---
+++ rstaurante/js/script.js
@@ -0,0 +1,16 @@
+
+alert("funciin");
+
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+});
+
+
+
+$(".burger").on("click", function(){
+    $(this).toggleClass("active");
+    $(".menu").toggleClass("open");
+  })
+
+
+  
